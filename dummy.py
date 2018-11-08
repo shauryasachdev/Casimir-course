05858922b710d93dd7b1800e8cc92ae5fd329a4b
a.moradi <amin.moradi.ph@hotmail.com>
2018-11-08 16:39:53 +0100

    This splits the image to 10 sub-images each includes one dish

---
+++ dummy.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import argparse
+import numpy as np
+import cv2
+
+img = mpimg.imread('img/results_L3.jpg')
+
+img1 = img[500:705, 75:300]
+img2 = img[500:705, 300:512]
+img3 = img[500:705, 512:720]
+img4 = img[495:700, 722:921]
+img5 = img[295:500, 80:300]
+img6 = img[295:500, 295:512]
+img7 = img[295:500, 512:709]
+img8 = img[295:495, 712:911]
+img9 = img[55:250, 575:775]
+img10 = img[60:255, 580:780]
