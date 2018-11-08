7ce5faebaf6afd8a97508a35d313c0ad3959ff37
Reza <r.aminihounejani@tudelft.nl>
2018-11-08 16:44:22 +0100

    changed image to grayscaled image

--- current/dummy.py
+++ current/dummy.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
+import cv2
 
-img = mpimg.imread('img/results_L3.jpg')
+img = mpimg.imread('img/gray.jpg')
 img1 = img[500:705, 75:300]
 img2 = img[500:705, 300:512]
 img3 = img[500:705, 512:720]
